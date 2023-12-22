3f8172e370fd5a3dc0176429ebbcbb15f7f49cde
thearzuyilmaz <the.arzu.yilmaz@gmail.com>
2023-12-22 16:59:03 +0100

    First Commit

---
+++ android/app/src/main/kotlin/arzuyilmaz/co/magic_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package arzuyilmaz.co.magic_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: BallPage(), //function değil widget
+    ),
+  ); //runapp
+}
+
+//build ediyoruz
+class BallPage extends StatelessWidget {
+  const BallPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.black,
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({Key? key}) : super(key: key);
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: (){
+          print('I got clicked');
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+    );
+  }
+}
+
